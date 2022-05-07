4a3b2398f7b7615c6fc0d06f9d303fb244d4149f
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2022-05-07 12:51:51 -0700

    Add counting functionality

--- counter/main.pl
+++ counter/main.pl
@@ -1,12 +1,26 @@
 :- use_module(library(pce)).
 
-:- initialization counter(_).
+:- initialization main.
 
-counter(Value) :-
+:- dynamic count/1.
+
+counter :-
     new(D, dialog('Counter')),
-    send(D, append, new(CountItem, text_item(count))),
-    send(D, append, button(ok, message(D, return, CountItem?selection))),
-    send(D, default_button(ok)),
+    count(Count),
+    send(D, append, new(CountItem, text_item(count, Count))),
+    send(D, append, button(ok, message(@prolog, update_count, CountItem))),
+    send(D, default_button, ok),
     get(D, confirm, Value),
-    free(D),
     Value \== @nil.
+
+update_count(CountItem) :-
+    get(CountItem, selection, Value),
+    retract(count(_)),
+    NewValue is Value + 1,
+    assert(count(NewValue)),
+    send(CountItem, clear),
+    send(CountItem, selection, NewValue).
+
+main:-
+    assert(count(0)),
+    counter.
